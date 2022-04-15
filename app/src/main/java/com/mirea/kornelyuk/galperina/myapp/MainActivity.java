d497a8c1294f91f5aeddd275786c0a42fb6713e5
Mr-Roman19 <roma.kornel004@mail.ru>
2022-04-16 01:39:47 +0300

    debug

--- app/src/main/java/com/mirea/kornelyuk/galperina/myapp/MainActivity.java
+++ app/src/main/java/com/mirea/kornelyuk/galperina/myapp/MainActivity.java
@@ -7,14 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.mirea.kornelyuk.galperina.myapp.adapter.CategoryAdapter;
+import com.mirea.kornelyuk.galperina.myapp.adapter.CourseAdapter;
 import com.mirea.kornelyuk.galperina.myapp.model.Category;
+import com.mirea.kornelyuk.galperina.myapp.model.Course;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView categoryRecycler;
+    RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
+    CourseAdapter courseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(7, "Выпечка"));
 
         setCategoryRecycler(categoryList);
+
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "sandv", "Бургеры",  "Новичок", "25 минут","#F0F8C1"));
+        courseList.add(new Course(2, "chickenplate", "Курица-ляванги азербайджанских евреев",  "Средний","1,5 часа" ,"#F0F8C1"));
+        courseList.add(new Course(3, "fish", "Гефильте Фиш",  "Продвинутый","3 часа", "#F0F8C1" ));
+        courseList.add(new Course(4,"soup", "Йеменский куриный суп",  "Средний","2,5 часа", "#F0F8C1"));
+        courseList.add(new Course(5, "salad", "Салат «Табуле» с петрушкой",  "Средний","25 минут", "#F0F8C1" ));
+        courseList.add(new Course(6, "bake", "Кугл с яблоками, изюмом и корицей",  "Средний", "1,5 часа","#F0F8C1" ));
+        courseList.add(new Course(7, "pie", "Нежная творожная запеканка", "Средний","1,5 часа", "#F0F8C1" ));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
     }
 
     private void setCategoryRecycler(List<Category> categoryList) {
--- app/src/main/java/com/mirea/kornelyuk/galperina/myapp/adapter/CourseAdapter.java
+++ app/src/main/java/com/mirea/kornelyuk/galperina/myapp/adapter/CourseAdapter.java
@@ -1,12 +1,18 @@
 package com.mirea.kornelyuk.galperina.myapp.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mirea.kornelyuk.galperina.myapp.R;
 import com.mirea.kornelyuk.galperina.myapp.model.Course;
 
 import java.util.List;
@@ -16,14 +22,26 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     Context context;
     List<Course> courses;
 
+    public CourseAdapter(Context context, List<Course> courses) {
+        this.context = context;
+        this.courses = courses;
+    }
+
     @NonNull
     @Override
     public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View courseItem = LayoutInflater.from(context).inflate(R.layout.course_item, parent, false);//для всех эл определенный дизайн
+        return new CourseAdapter.CourseViewHolder(courseItem);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+        holder.courseBg.setBackgroundColor(Color.parseColor(courses.get(position).getColour())); //цвет фона
+        int imageId = context.getResources().getIdentifier("ic_" + courses.get(position).getImg(), "drawable", context.getPackageName()); //получаем картинку из строки в id
+        holder.courseImage.setImageResource(imageId); //изображение
+        holder.courseTitle.setText(courses.get(position).getTitle());
+        holder.courseData.setText(courses.get(position).getDate());
+        holder.courseLvl.setText(courses.get(position).getLvl());
 
     }
 
@@ -34,9 +52,18 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     }
 
     public  static final class CourseViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout courseBg;
+        ImageView courseImage;
+        TextView courseTitle, courseLvl, courseData;
 
         public CourseViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            courseBg = itemView.findViewById(R.id.courseBg);
+            courseImage = itemView.findViewById(R.id.courseImage);
+            courseTitle = itemView.findViewById(R.id.courseTitle);
+            courseData = itemView.findViewById(R.id.courseDate);
+            courseLvl = itemView.findViewById(R.id.courseLvl);
         }
     }
 }
